bee0be0207a37bdb3555b898540cebdae5c0184f
RedjBonifacio <qrgfbonifacio@tip.edu.ph>
2024-08-22 12:29:34 +0800

    Update Exercise2 BONIFACIO.py

--- Exercise2 BONIFACIO.py
+++ Exercise2 BONIFACIO.py
@@ -1,8 +1,14 @@
 grade = int(input("What is your grade?: "))
 
-if grade >= 75:
-    print('You passed')
+if grade >= 75 and grade <= 100:
+    print('\nYou passed')
+
 elif grade == 74:
-    print('You will take remedial')
-elif grade < 74:
-    print('You failed')
+    print('\nYou failed')
+elif grade < 74 and grade > 0:
+    print('\nYou will take remedial')
+else:
+    print('==========================================')
+    print('Must input 0-100')
+    print('Do not input negative numbers')
+    print('==========================================')
